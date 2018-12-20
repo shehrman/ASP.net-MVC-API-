73e6d07328a1c981761b6512140b74a807f873c7
Ibrahim, Shehrman <shehrman.ibrahim@cgi.com>
2018-12-20 08:28:47 +0100

    Api with Sql Server

---
+++ WebAPIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using EmploeeyDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public List<Employees> Get()
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                return db.Employees.ToList();
+            }
+            
+        }
+    }
+}
